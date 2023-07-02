4ef181ec4d4846c05472e4c8af92ae32ed8d8e12
IvanGuculyak <ivan.guculyak@gmail.com>
2023-07-02 22:59:15 +0300

    Updated settings.py

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "todo-as7t.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
@@ -143,6 +143,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+STATIC_ROOT = "staticfiles/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
