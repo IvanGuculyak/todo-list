3f1f77d5abe746626d287c4fb15eb498b93b0615
IvanGuculyak <ivan.guculyak@gmail.com>
2023-07-02 11:35:29 +0300

    added views of Tag and Task

--- service/views.py
+++ service/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from service.models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    queryset = Task.objects.all().select_related("tags")
+    paginate_by = 3
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    paginate_by = 5
